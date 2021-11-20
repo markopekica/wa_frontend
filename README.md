eb40fec951f0f70d1db7d08b11d357ce42ec0db9
marko <pekica.marko@gmail.com>
2021-11-20 13:12:56 +0100

    very basic navigationBar skeleton

--- README.md
+++ README.md
@@ -17,3 +17,18 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+#### Build log
+
+##### navigationBar - 19.11.2021.
+
+- added component NavigationBar.vue
+- using [bootstrap icons](https://icons.getbootstrap.com)
+
+###### navigationBar - 20.11.2021.
+
+- added *sign out*, *info* icons
+- adjusting for portrait (mobile) view
+- **!!** don't like how *sign out* might be too close to other icons
+  - might be pressed accidentally
+- **!!** on portrait between 350px and 422px width it does not look very nice
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -4,14 +4,23 @@
       <i class="bi bi-triangle"></i>
     </div>
     <div class="menu">
-      <router-link class="rlink" to="/activity">
+      <router-link class="rlink" to="/activity" title="activity list">
         <i class="bi bi-list-task"></i
       ></router-link>
-      <router-link class="rlink" to="/about"
+      <router-link class="rlink" to="/timer" title="timer"
         ><i class="bi bi-stopwatch"></i
       ></router-link>
-      <router-link class="rlink" to="/">
+      <router-link class="rlink" to="/" title="stats">
         <i class="bi bi-graph-up"></i>
+      </router-link>
+    </div>
+    <!-- other icons; sign out, info -->
+    <div class="other-icons">
+      <router-link class="rlink" to="" title="sign out">
+        <i class="bi bi-box-arrow-left"></i>
+      </router-link>
+      <router-link class="rlink" to="about" title="info">
+        <i class="bi bi-info-circle"></i>
       </router-link>
     </div>
   </div>
@@ -25,36 +34,61 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.nav {
-  border-right: 1px solid #111;
-  float: left;
-  min-height: 100vh;
-  width: 42px;
-  position: fixed;
-  padding: 0.5em;
-  /* background-color: #111;
-    color: ghostwhite; */
-}
-.logo {
-    padding: .5em;
-  margin-bottom: 2em;
-  /* border: 1px solid green; */
-}
-.menu {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 .rlink {
-    /* invisible border stops jumping in case a border appears on hover */
+  /* invisible border stops jumping in case a border appears on hover */
   border: 1px solid white;
   padding: 0.5em;
   margin: 0.5em;
   max-width: 32px;
 }
 .rlink:hover {
-  /* cursor: pointer; */
   border: 1px solid pink;
   border-radius: 6px;
+}
+@media only screen and (orientation: portrait) {
+  .nav {
+    border-bottom: 1px solid #111;
+    div {
+      line-height: 2.5em;
+    }
+    display: flex;
+    padding: 0.75em 0.25em;
+    justify-content: space-between;
+  }
+  .logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 0.5em;
+    box-shadow: 1px 0px 0px lightgray;
+    width: 25%;
+  }
+  .other-icons {
+    box-shadow: -1px 0px 0px lightgray;
+    width: 25%;
+  }
+}
+@media only screen and (orientation: landscape) {
+  .nav {
+    border-right: 1px solid #111;
+    float: left;
+    min-height: 100vh;
+    width: 42px;
+    position: fixed;
+    padding: 0.5em;
+  }
+  .logo {
+    padding: 0.5em;
+    margin-bottom: 2em;
+  }
+  .menu,
+  .other-icons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .other-icons {
+    box-shadow: 0px -1px 0px lightgray;
+  }
 }
 </style>
